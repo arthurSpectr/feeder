ed39175ec90b265de8422e917fa54369d638f8f1
arturtyan <artur.tyan93@gmail.com>
2023-03-20 16:42:15 -0400

    init commit

---
+++ src/test/java/regexit/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package regexit.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
